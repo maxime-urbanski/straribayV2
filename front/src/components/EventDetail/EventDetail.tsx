2841c0ba9d6b438595d5ce6c36429caa0f4de0ff
Fritsch <v.fritsch1981@gmail.com>
2021-02-04 16:57:06 +0100

    Try to install mutation

--- front/src/components/EventDetail/EventDetail.tsx
+++ front/src/components/EventDetail/EventDetail.tsx
@@ -5,25 +5,52 @@ import { CreationContainer } from "../../styles/containers";
 import { Title, Button } from "../../styles/elements";
 import eventList from "../../Data";
 
-import { ApolloClient, InMemoryCache, UseQuery } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
+
+const Add_Event = gql`
+mutation AddEvent($title: String!, $date: Time!, $hour: String, $author: String, $description: String, $infos: String, $image: Sring) {
+  addEvent(title: $title, date: $date, hour: $hour, author: $author, description: $description, infos: $infos, image: $image) {
+    title
+    date
+    hour
+    author
+    description
+    infos
+    image
+  }
+} 
+`
 
 const Details = (props: any) => {
-  // eslint-disable-next-line react/destructuring-assignment
-  const { attending, userSelected, event } = props.location.state;
-  const { title, description, theme, date, hour, info } = event;
-  event.id = eventList.length + 1;
+    const { attending, userSelected, event } = props.location.state;
+    const { title, description, theme, date, hour, info } = event;
+      event.id = eventList.length + 1;
+    // eslint-disable-next-line react/destructuring-assignment
+    const [addEvent, {data}] = useMutation(Add_Event);
 
-  const addNewEvent = () => {
-    eventList.push(event);
-  };
+    console.log(title);
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
+      e.preventDefault();
+      addEvent({variables: {
+        title: title,
+        date: date,
+        hour: hour,
+        author: "Vince",
+        description: description,
+        infos: info,
+        image: "test image"
+      }})      
+    } 
+    ;
+  console.log({data});
 
   return (
     <>
       <Title>Create your Event</Title>
       <CreationContainer>
-        <h3>{title}</h3>
-        <h3>{theme}</h3>
-        <h3>{description}</h3>
+        <h3>{data && data.title}</h3>
+        <h3>{data && data.theme}</h3>
+        <h3>{data && data.description}</h3>
         <h3>{date}</h3>
         <h3>{hour}</h3>
         <h3>{info}</h3>
@@ -43,7 +70,7 @@ const Details = (props: any) => {
             },
           }}
         >
-          <Button onClick={addNewEvent}>Add this event ?</Button>
+          <Button onSubmit={handleSubmit}>Add this event ?</Button>
         </Link>
       </CreationContainer>
     </>
